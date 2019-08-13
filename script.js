fd3ba4b5a1b9d8682cbd204eb48054c863d5da9c
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-08-13 21:36:05 +0100

    adding mouse movements

--- script.js
+++ script.js
@@ -1,2 +1,62 @@
+//get start button
+const startBtn = document.querySelector('.btnStart');
 
-const Startbtn = document.querySelector
+const gameOver = document.getElementById('gameOver');
+
+const container = document.getElementById('container');
+
+let gamePlay = false;
+
+//Create player object
+let player;
+
+//to animate the frame constantly
+let animateGame;
+
+
+//add a mousedown Event to shoot
+const mouseDown = (e) =>{
+    if(gamePlay){
+        console.log('FIRE');
+    }
+
+}
+
+container.addEventListener('mousedown', mouseDown);
+
+
+
+
+//start game button functionality
+const startGame=()=>{
+    gamePlay = true;
+    gameOver.style.display = 'none';
+
+    player = {
+        score: 0,
+        barwidth:100,
+        lives:100
+    }
+
+    //start game  session
+    const gameSession = () =>{
+        if(gamePlay){
+            //move shots
+            //update dashboard
+            //move enemy
+        animateGame = requestAnimationFrame(gameSession);
+    }
+}
+
+    animateGame = requestAnimationFrame(gameSession);
+
+
+}
+
+
+
+startBtn.addEventListener('click', startGame);
+
+
+
+
