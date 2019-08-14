fcdd295a3e9642abbdf64d155c490d974a3c72da
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-08-14 14:41:06 +0100

    Shots are working, but going opposite... chang turrets css

--- script.js
+++ script.js
@@ -19,29 +19,53 @@ const box = document.querySelector('.base');
 const tanker = document.querySelector('.box');
 const turet = document.querySelector('.turets');
 
+
+
 //use to get middle point(axis) of the tank
 const boxCenter = [box.offsetLeft + (box.offsetWidth / 2),
     box.offsetTop + (box.offsetHeight / 2)
 ];
 //access by: boxCenter[0],boxCenter[1]
 
-console.log(boxCenter)
 
 
-//add a mousedown Event to shoot
-const mouseDown = (e) => {
-    if (gamePlay) {
-        console.log('FIRE');
-    }
-}
 
-
-//to get degrees
+    //to get degrees
 const getDeg = (e) => {
     let angle = Math.atan2(e.clientX - boxCenter[0], -(e.clientY - boxCenter[1]));
     //multiply by 180/pi to get angles
     return angle * (180 / Math.PI);
 }
+
+//get degrees to show where bullets go
+const degRadius=(deg) =>{
+    return deg*(Math.PI/180);
+    }
+
+//add a mousedown Event to shoot
+const mouseDown = (e) => {
+    if (gamePlay) {
+        //create bullets
+        console.log('fire');
+        let div = document.createElement('div');
+
+        let deg = getDeg(e);
+        div.setAttribute('class', 'bullets')
+
+        div.movey = -5 * Math.sin(degRadius(deg));
+        div.movex = 5 * Math.cos(degRadius(deg));
+        div.style.left = (boxCenter[0]-6) + 'px';
+        div.style.top = (boxCenter[1]-6) + 'px';
+        div.style.width = 10 + 'px';
+        div.style.height = 10 + 'px';
+
+        //add to container element
+        container.appendChild(div);
+    }
+}
+
+
+
 
 const mousePosition = (e) => {
     let deg = getDeg(e)
@@ -54,6 +78,7 @@ const mousePosition = (e) => {
     box.style.oTransform = `rotate(${deg}deg)`;
 
 }
+
 
 container.addEventListener('mousemove', mousePosition);
 container.addEventListener('mousedown', mouseDown);
@@ -72,12 +97,26 @@ const startGame = () => {
         lives: 100
     }
 
+    const moveShots=()=>{
+        let shots = document.querySelectorAll('.bullets');
+
+        for (let shot of shots){
+            shot.style.top = shot.offsetTop + shot.movex + 'px';
+            shot.style.left = shot.offsetLeft + shot.movey + 'px';
+        }
+    }
+
+
+
     //start game  session
     const gameSession = () => {
         if (gamePlay) {
 
             //move shots
+            moveShots()
+
             //update dashboard
+
             //move enemy
 
             //Change tank color
@@ -91,8 +130,6 @@ const startGame = () => {
 
     //Similar to setinterval()
     animateGame = requestAnimationFrame(gameSession);
-
-
 }
 
 
