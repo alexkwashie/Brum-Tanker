52e1986d629dd45cf6f269bae152312c48271d12
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-08-14 10:45:57 +0100

    getting center of tanks, Maths , degrees and angles

--- script.js
+++ script.js
@@ -13,41 +13,63 @@ let player;
 //to animate the frame constantly
 let animateGame;
 
+//Get center point of tank
+const box = document.querySelector('.box');
+
+const boxCenter = [box.offsetLeft + (box.offsetWidth / 2),
+    box.offsetTop + (box.offsetHeight / 2)
+];
+//access by: boxCenter[0],boxCenter[1]
+
 
 //add a mousedown Event to shoot
-const mouseDown = (e) =>{
-    if(gamePlay){
+const mouseDown = (e) => {
+    if (gamePlay) {
         console.log('FIRE');
     }
-
 }
 
+
+//to get degrees
+const getDeg = (e) => {
+    let angle = Math.atan2(e.clientX - boxCenter[0], -(e.clientY - boxCenter[1]));
+    //multiply by 180/pi to get angles
+    return angle * (180 / Math.PI);
+}
+
+const mousePosition = (e) => {
+    let mouseAngle = getDeg(e)
+    console.log(mouseAngle);
+}
+
+container.addEventListener('mousemove', mousePosition);
 container.addEventListener('mousedown', mouseDown);
 
 
 
 
 //start game button functionality
-const startGame=()=>{
+const startGame = () => {
     gamePlay = true;
     gameOver.style.display = 'none';
 
     player = {
         score: 0,
-        barwidth:100,
-        lives:100
+        barwidth: 100,
+        lives: 100
     }
 
     //start game  session
-    const gameSession = () =>{
-        if(gamePlay){
+    const gameSession = () => {
+        if (gamePlay) {
             //move shots
             //update dashboard
             //move enemy
-        animateGame = requestAnimationFrame(gameSession);
+            animateGame = requestAnimationFrame(gameSession);
+        }
     }
-}
 
+    //Similar to setinterval()
     animateGame = requestAnimationFrame(gameSession);
 
 
@@ -56,7 +78,3 @@ const startGame=()=>{
 
 
 startBtn.addEventListener('click', startGame);
-
-
-
-
